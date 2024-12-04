0584c010d14ca9422bacc55e01726a4767f335e0
Kishan S Patel <bishan.batel@protonmail.com>
2024-12-03 19:08:33 -0800

    Document

--- memfun.adapter.h
+++ memfun.adapter.h
@@ -1,16 +1,29 @@
+/**
+ * name: Kishan S Patel
+ * id: kishan.patel
+ */
 #ifndef MEMFUN_ADAPTER_H
 #define MEMFUN_ADAPTER_H
+
+/**
+ * @brief Functor used as a conversion from a class's non const method to a function with a 'this' parameter
+ */
 template<typename RetType, typename ClassType, typename ArgType>
 class MFA_1arg {
   RetType (ClassType::*method)(ArgType);
+
 public:
   MFA_1arg(RetType (ClassType::*pointer)(ArgType)) : method(pointer) {}
 
-  RetType operator()(ClassType &type, ArgType arg) const { return (type.*method)(arg); }
+  /**
+   * @brief Calls the method on the instance 'self'
+   */
+  RetType operator()(ClassType &self, ArgType arg) const { return (self.*method)(arg); }
 };
 
-// helper functions
-// for non-const member function
+/**
+ * @brief Creates a functor used as a conversion from a class's non const method to a function with a 'this' parameter
+ */
 template<typename RetType, typename ClassType, typename ArgType>
 MFA_1arg<RetType, ClassType, ArgType> mfp_1arg(RetType (ClassType::*pointer)(ArgType)) {
   return MFA_1arg<RetType, ClassType, ArgType>(pointer);
